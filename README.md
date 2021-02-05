df7e2284b7d8927d9fea255d170040a047453f6d
wandji20 <wbertrand10@gmail.com>
2021-02-05 18:02:40 +0100

    Update readme and add post and member authentication

--- README.md
+++ README.md
@@ -74,4 +74,4 @@ Give a ⭐️ if you like this project!
 
 ## 📝 License
 
-This project is [MIT](./LICENSE) licensed.
+This project is [MIT](/license.txt) licensed.
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
-  before_action :authenticate_member!, except: %i[index show]
+  before_action :authenticate_member!, only: %i[new create]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
